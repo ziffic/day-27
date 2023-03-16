2121adf45ee0ca2a4a4b3f5a3df12c7dc2680ada
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-16 12:37:23 -0700

    tkinter layout

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from tkinter import *
 
-window = Tk()
-window.title("GUI Program")
-window.minsize(width=500, height=300)
-
-my_label = Label(text="I am a label", font=("Arial", 24, "normal"))
-my_label.pack()
-
-my_label["text"] = "New text"
-
 
 def button_clicked():
     my_label.config(text=input.get())
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+window = Tk()
+window.title("GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "normal"))
+my_label["text"] = "New text"
+# my_label.pack()
+# my_label.place(x=100, y=200)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+# Button
+button = Button(text="Click Me", command=button_clicked)
+# button.pack()
+button.grid(column=1, row=1)
+
+# Button2
+button2 = Button(text="New Button", command=button_clicked)
+# button2.pack()
+button2.grid(column=2, row=0)
+
+# Entry
 input = Entry(width=10)
 print(input.get())
-input.pack()
-
+# input.pack()
+input.grid(column=3, row=2)
 
 window.mainloop()
