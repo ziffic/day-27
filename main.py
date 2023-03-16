f006009347d1ec0995ae223d7b622d3150a49d41
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-16 11:54:51 -0700

    basic tkinter functions

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "normal"))
-my_label.pack(side="left")
+my_label = Label(text="I am a label", font=("Arial", 24, "normal"))
+my_label.pack()
+
+my_label["text"] = "New text"
+
+
+def button_clicked():
+    my_label.config(text=input.get())
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+input = Entry(width=10)
+input.pack()
+
 
 window.mainloop()
--- playground.py
+++ playground.py
@@ -6,3 +6,20 @@ def add(*arg):
 
 
 print(add(1, 2, 1, 1, 1))
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiple"]
+    print(n)
+
+
+calculate(2, add=3, multiple=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("colors")
+        self.seats = kw.get("seats")
